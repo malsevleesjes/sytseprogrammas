879ac98df3d0f14ac4a5841e58e410a92abcbd92
Sytse de malse vleesje <malsevleesjesakaSytse@Mini-van-Apple.fritz.box>
2020-03-17 12:09:51 +0100

    (kaas)

---
+++ components/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+    <h1>comments:</h1>
+    <div className="comment">
+    <a href="/" className="avatar">
+    <img alt="avatar" src= {faker.image.avatar()} />
+    </a>
+    <div className="content">
+    <a href="/" className="author>">
+    sam
+    </a>
+    <div className="metadata">
+    <span className="data">Today at 00:00AM</span>
+    </div>
+    <div className="text">nice body!</div>
+    </div>
+    </div>
+      <div className="comment">
+      <a href="/" className="avatar">
+      <img alt="avatar" src= {faker.image.avatar()} />
+      </a>
+      <div className="content">
+      <a href="/" className="author>">
+      sam
+      </a>
+      <div className="metadata">
+      <span className="data">Today at 00:00AM</span>
+      </div>
+      <div className="text">nice body!</div>
+      </div>
+      </div>  <div className="comment">
+        <a href="/" className="avatar">
+        <img alt="avatar" src= {faker.image.avatar()} />
+        </a>
+        <div className="content">
+        <a href="/" className="author>">
+        sam
+        </a>
+        <div className="metadata">
+        <span className="data">Today at 00:00AM</span>
+        </div>
+        <div className="text">nice body!</div>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
