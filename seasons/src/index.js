eb9663a3a026747395fd15faf7e87f4aa5a8b206
Sytse de malse vleesje <malsevleesjesakaSytse@Mini-van-Apple.fritz.box>
2020-03-25 16:01:00 +0100

    klonks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,21 +5,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+// THIS IS THE ONLY TIME we do direckt assingment
+// to this.state
 this.state = { lat: null };
+
+window.navigator.geolocation.getCurrentPosition(
+  (position) => {
+    // we called setState!!!!
+    this.setState({ lat: position.coords.latitude });
+  },
+  (err) => console.log(err)
+);
 }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: {this.state.lat} </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'))
+;
